ace3ab4c2ee4ffaa464c235dfc5e3d27d5e44952
roccosalazar <rf.salazar@alcampus.it>
2024-12-22 20:09:12 +0100

    Add project setup script and data to process

---
+++ project_setup.py
@@ -0,0 +1,31 @@
+from itertools import product
+
+# Sostituisci <Group_Number> con il numero del tuo gruppo
+i = 3
+
+# Definizione delle opzioni
+data = [
+    "version_1.csv", 
+    "version_2.xlsx", 
+    "version_3.txt", 
+    "version_4.json", 
+    "version_5.tsv"
+]
+tech = [
+    "Random Subsampling", 
+    "K-fold Cross Validation", 
+    "Leave-one-out Cross Validation", 
+    "Leave-p-out Cross Validation", 
+    "Stratified Cross Validation", 
+    "Stratified Shuffle Split", 
+    "Bootstrap"
+]
+
+# Generazione delle combinazioni e selezione
+A, B, C = list(product(data, tech, tech))[i]
+print("A:", A, "B:", B, "C:", C)
+
+"""
+Output: 
+A: version_1.csv B: Random Subsampling C: Leave-p-out Cross Validation
+"""
